79f5e7a3d0f2a8484c41c4aae6bbcb9ff45b9000
Eight0 <dysonshu@gmail.com>
2017-06-12 15:26:33 +0800

    init

---
+++ resources/assets/js/components/demo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Demo</div>
+
+                    <div class="panel-body">
+                        <div id="demo">
+                            <p>{{ message }}</p>
+                            <input v-model="message">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        data: function () {
+            return {
+                    message: 'this is a message.'
+            }
+        }
+    }
+</script>
